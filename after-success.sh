e478130f89bd2cc73fa1cd48253bd1c73bd1f169
Yuri Brito <yuriabrito@gmail.com>
2017-10-31 14:51:26 -0200

    envvars and pushing to helm-repo

--- after-success.sh
+++ after-success.sh
@@ -22,9 +22,11 @@ if [ $TRAVIS_PULL_REQUEST = 'false' ]; then
 
     # creating helm chart for this tag
     cd helm; make package
-    git clone git@git.topfreegames.com:topfreegames/helm-repo.git
+    ssh-keyscan $TFG_GITLAB >> ~/.ssh/known_hosts
+    git clone $HELM_REPO_GIT
     cp -r helm/pkg helm-repo/charts
     cd helm-repo; make vendor; make upload-with-env
+    cd helm-repo; git add .; git commit -m "travis helm update"; git push origin master
   fi
 fi
 
