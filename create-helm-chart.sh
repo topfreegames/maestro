b6ffa0ad528e829f714e5c3883bb5d2c626e9013
Yuri Brito <yuriabrito@gmail.com>
2017-11-07 11:39:06 -0200

    showing output from create-helm-chart

--- create-helm-chart.sh
+++ create-helm-chart.sh
@@ -1,8 +1,8 @@
 if ! test -z $TRAVIS_TAG; then
   cd helm; make package; cd ..
   ssh-keyscan $TFG_GITLAB >> ~/.ssh/known_hosts 
-  git clone $HELM_REPO_GIT > /dev/null 2>&1
+  git clone $HELM_REPO_GIT
   cp -r \helm/pkg/* helm-repo/charts
-  cd helm-repo; make vendor; make upload-with-env > /dev/null 2>&1
-  git add .; git commit -m "travis helm update"; git push origin master > /dev/null 2>&1
+  cd helm-repo; make vendor; make upload-with-env
+  git add .; git commit -m "travis helm update"; git push origin master
 fi
