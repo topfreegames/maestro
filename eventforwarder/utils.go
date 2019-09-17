8fdf7ac35877fe78881e29d05831079385e11717
lftakakura <lftakakura@gmail.com>
2019-09-17 11:20:05 -0300

    Copy metadata to avoid race condition in forwarding (#81)

---
+++ eventforwarder/utils.go
@@ -0,0 +1,16 @@
+package eventforwarder
+
+// CopyMap Deep copies a map[string]interface
+func CopyMap(m map[string]interface{}) map[string]interface{} {
+	cp := make(map[string]interface{})
+	for k, v := range m {
+		vm, ok := v.(map[string]interface{})
+		if ok {
+			cp[k] = CopyMap(vm)
+		} else {
+			cp[k] = v
+		}
+	}
+
+	return cp
+}
