61ea5be66a5b5c310949e678162e3c1a91138d68
Henrique Rodrigues <henrique.93.rodrigues@gmail.com>
2019-09-27 22:40:15 -0300

    Revert "watch resyncPeriod to 1 min"
    This reverts commit 8809586f98dcb5329ef914872b0c2f1439054338.

--- watcher/watcher.go
+++ watcher/watcher.go
@@ -1489,7 +1489,7 @@ func (w *Watcher) configureKubeWatch() (cache.Controller, chan struct{}) {
 	_, controller := cache.NewInformer(
 		watchlist,
 		&v1.Pod{},
-		time.Minute,
+		0,
 		cache.ResourceEventHandlerFuncs{
 			// --- Kube Create
 			AddFunc: func(obj interface{}) {
