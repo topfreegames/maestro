63398273b3136072b64f0b54b949435b764a8e67
Luiz Miranda <luiz.miranda@wildlifestudios.com>
2021-07-27 22:07:16 -0300

    Add ping handler test

---
+++ internal/handlers/ping_handler_test.go
@@ -0,0 +1,100 @@
+//+build integration
+
+package handlers
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
+	"github.com/stretchr/testify/require"
+	configmock "github.com/topfreegames/maestro/internal/config/mock"
+	"github.com/topfreegames/maestro/protogen/api"
+)
+
+func TestPingHandler(t *testing.T) {
+
+	t.Run("with valid request", func(t *testing.T) {
+
+		mockCtrl := gomock.NewController(t)
+		defer mockCtrl.Finish()
+		config := configmock.NewMockConfig(mockCtrl)
+
+		config.EXPECT().GetBool("management_api.enable").Return(true).AnyTimes()
+		config.EXPECT().GetString("management_api.port").Return("8081").AnyTimes()
+		config.EXPECT().GetString("management_api.gracefulShutdownTimeout").Return("10000").AnyTimes()
+
+		mux := runtime.NewServeMux()
+		api.RegisterPingHandlerServer(context.Background(), mux, ProvidePingHandler())
+
+		req, err := http.NewRequest("GET", "/ping", nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		rr := httptest.NewRecorder()
+
+		mux.ServeHTTP(rr, req)
+
+		require.Equal(t, 200, rr.Code)
+		bodyString := rr.Body.String()
+		require.Equal(t, "{\"message\":\"pong\"}", bodyString)
+	})
+
+	t.Run("with invalid request method", func(t *testing.T) {
+
+		mockCtrl := gomock.NewController(t)
+		defer mockCtrl.Finish()
+		config := configmock.NewMockConfig(mockCtrl)
+
+		config.EXPECT().GetBool("management_api.enable").Return(true).AnyTimes()
+		config.EXPECT().GetString("management_api.port").Return("8081").AnyTimes()
+		config.EXPECT().GetString("management_api.gracefulShutdownTimeout").Return("10000").AnyTimes()
+
+		mux := runtime.NewServeMux()
+		api.RegisterPingHandlerServer(context.Background(), mux, ProvidePingHandler())
+
+		req, err := http.NewRequest("POST", "/ping", nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		rr := httptest.NewRecorder()
+
+		mux.ServeHTTP(rr, req)
+
+		require.Equal(t, 405, rr.Code)
+		bodyString := rr.Body.String()
+		require.Equal(t, "Method Not Allowed\n", bodyString)
+	})
+
+	t.Run("with invalid request path", func(t *testing.T) {
+
+		mockCtrl := gomock.NewController(t)
+		defer mockCtrl.Finish()
+		config := configmock.NewMockConfig(mockCtrl)
+
+		config.EXPECT().GetBool("management_api.enable").Return(true).AnyTimes()
+		config.EXPECT().GetString("management_api.port").Return("8081").AnyTimes()
+		config.EXPECT().GetString("management_api.gracefulShutdownTimeout").Return("10000").AnyTimes()
+
+		mux := runtime.NewServeMux()
+		api.RegisterPingHandlerServer(context.Background(), mux, ProvidePingHandler())
+
+		req, err := http.NewRequest("GET", "/pong", nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		rr := httptest.NewRecorder()
+
+		mux.ServeHTTP(rr, req)
+
+		require.Equal(t, 404, rr.Code)
+		bodyString := rr.Body.String()
+		require.Equal(t, "Not Found\n", bodyString)
+	})
+}
