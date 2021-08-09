ebd8719471128add41ddb2622cfcb27524ffa958
gabrielcorado <gabrielcorado@mail.com>
2021-08-09 13:20:52 -0300

    feat(makefile): move code generation to gen package

--- Makefile
+++ Makefile
@@ -5,10 +5,7 @@ SOURCES := $(shell \
 .PHONY: deps
 deps:
 	@go get ./...
-
-.PHONY: tools
-tools:
-	@go install ./...
+	@go mod download
 
 ################################################################################
 ## Lint and tests
@@ -63,23 +60,6 @@ run/management-api: build/management-api
 .PHONY: generate
 generate:
 	@go generate ./gen
-
-.PHONY: wire
-wire:
-	@go run github.com/google/wire/cmd/wire ./...
-
-.PHONY: mocks
-mocks:
-	@mockgen -source=internal/core/ports/port_allocator.go -destination=internal/adapters/port_allocator/mock/mock.go -package=mock
-	@mockgen -source=internal/core/ports/runtime.go -destination=internal/adapters/runtime/mock/mock.go -package=mock
-	@mockgen -source=internal/core/ports/room_storage.go -destination=internal/adapters/room_storage/mock/mock.go -package=mock
-	@mockgen -source=internal/core/ports/instance_storage.go -destination=internal/adapters/instance_storage/mock/mock.go -package=mock
-	@mockgen -source=internal/core/ports/operation_storage.go -destination=internal/adapters/operation_storage/mock/mock.go -package=mock
-	@mockgen -source=internal/core/ports/scheduler_storage.go -destination=internal/adapters/scheduler_storage/mock/mock.go -package=mock
-	@mockgen -source=internal/core/ports/operation_flow.go -destination=internal/adapters/operation_flow/mock/mock.go -package=mock
-	@mockgen -source=internal/config/config.go -destination=internal/config/mock/mock.go -package=mock
-	@mockgen -source=internal/core/operations/definition.go -destination=internal/core/operations/mock/definition.go -package=mock
-	@mockgen -source=internal/core/operations/executor.go -destination=internal/core/operations/mock/executor.go -package=mock
 
 ################################################################################
 ## Migration and database make targets
--- gen/gen.go
+++ gen/gen.go
@@ -22,5 +22,23 @@
 
 package gen
 
+// Protobuf
 //go:generate go run github.com/bufbuild/buf/cmd/buf generate --template ../buf.gen.yaml --output ../ --path ../api/proto/v1 ../
-//go:generate go run github.com/google/addlicense -v -skip yaml -skip proto -f ../LICENSE ../
+
+// Wire
+//go:generate go run github.com/google/wire/cmd/wire ./../...
+
+// Mocks
+//go:generate go run github.com/golang/mock/mockgen -source=../internal/core/ports/port_allocator.go -destination=../internal/adapters/port_allocator/mock/mock.go -package=mock
+//go:generate go run github.com/golang/mock/mockgen -source=../internal/core/ports/runtime.go -destination=../internal/adapters/runtime/mock/mock.go -package=mock
+//go:generate go run github.com/golang/mock/mockgen -source=../internal/core/ports/room_storage.go -destination=../internal/adapters/room_storage/mock/mock.go -package=mock
+//go:generate go run github.com/golang/mock/mockgen -source=../internal/core/ports/instance_storage.go -destination=../internal/adapters/instance_storage/mock/mock.go -package=mock
+//go:generate go run github.com/golang/mock/mockgen -source=../internal/core/ports/operation_storage.go -destination=../internal/adapters/operation_storage/mock/mock.go -package=mock
+//go:generate go run github.com/golang/mock/mockgen -source=../internal/core/ports/scheduler_storage.go -destination=../internal/adapters/scheduler_storage/mock/mock.go -package=mock
+//go:generate go run github.com/golang/mock/mockgen -source=../internal/core/ports/operation_flow.go -destination=../internal/adapters/operation_flow/mock/mock.go -package=mock
+//go:generate go run github.com/golang/mock/mockgen -source=../internal/config/config.go -destination=../internal/config/mock/mock.go -package=mock
+//go:generate go run github.com/golang/mock/mockgen -source=../internal/core/operations/definition.go -destination=../internal/core/operations/mock/definition.go -package=mock
+//go:generate go run github.com/golang/mock/mockgen -source=../internal/core/operations/executor.go -destination=../internal/core/operations/mock/executor.go -package=mock
+
+// License
+//go:generate go run github.com/google/addlicense -v -skip yml -skip yaml -skip proto -f ../LICENSE ../
