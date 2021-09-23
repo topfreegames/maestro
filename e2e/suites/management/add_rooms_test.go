c350ce2b6afe32d77b13217247712513aa862802
GuilhermBrSp <guilhermbrsp@gmail.com>
2021-09-23 10:43:16 -0300

    Refactor add rooms tests

--- e2e/suites/management/add_rooms_test.go
+++ e2e/suites/management/add_rooms_test.go
@@ -48,73 +48,15 @@ func TestAddRooms(t *testing.T) {
 		roomsStorage := roomStorageRedis.NewRedisStateStorage(redisClient)
 
 		t.Run("when created rooms does not reply its state back then it finishes the operation successfully", func(t *testing.T) {
-			schedulerName := framework.GenerateSchedulerName()
-			createRequest := &maestroApiV1.CreateSchedulerRequest{
-				Name:                   schedulerName,
-				Game:                   "test",
-				Version:                "v1.1",
-				TerminationGracePeriod: 15,
-				Containers: []*maestroApiV1.Container{
-					{
-						Name:            "example",
-						Image:           "alpine",
-						Command:         []string{"sh", "-c", "tail -f /dev/null"},
-						ImagePullPolicy: "Always",
-						Requests: &maestroApiV1.ContainerResources{
-							Memory: "20Mi",
-							Cpu:    "10m",
-						},
-						Limits: &maestroApiV1.ContainerResources{
-							Memory: "20Mi",
-							Cpu:    "10m",
-						},
-						Ports: []*maestroApiV1.ContainerPort{
-							{
-								Name:     "default",
-								Protocol: "tcp",
-								Port:     80,
-							},
-						},
-					},
-				},
-			}
+			schedulerName, err := createSchedulerAndWaitForIt(
+				t,
+				maestro,
+				apiClient,
+				kubeclient,
+				[]string{"sh", "-c", "tail -f /dev/null"},
+			)
 
-			createResponse := &maestroApiV1.CreateSchedulerResponse{}
-			err := apiClient.Do("POST", "/schedulers", createRequest, createResponse)
-			require.NoError(t, err)
-
-			require.Eventually(t, func() bool {
-				listOperationsRequest := &maestroApiV1.ListOperationsRequest{}
-				listOperationsResponse := &maestroApiV1.ListOperationsResponse{}
-				err = apiClient.Do("GET", fmt.Sprintf("/schedulers/%s/operations", schedulerName), listOperationsRequest, listOperationsResponse)
-				require.NoError(t, err)
-
-				if len(listOperationsResponse.FinishedOperations) == 0 {
-					return false
-				}
-
-				require.Equal(t, "create_scheduler", listOperationsResponse.FinishedOperations[0].DefinitionName)
-				require.Equal(t, "finished", listOperationsResponse.FinishedOperations[0].Status)
-				return true
-			}, 30*time.Second, time.Second)
-
-			_, err = kubeclient.CoreV1().Namespaces().Get(context.Background(), schedulerName, metav1.GetOptions{})
-			require.NoError(t, err)
-
-			// wait for service account to be created
-			// TODO: check if we need to wait the service account to be created on internal/adapters/runtime/kubernetes/scheduler.go
-			// we were having errors when not waiting for this in this test, reported in this issue https://github.com/kubernetes/kubernetes/issues/66689
-			require.Eventually(t, func() bool {
-				svcAccs, err := kubeclient.CoreV1().ServiceAccounts(schedulerName).List(context.Background(), metav1.ListOptions{})
-				require.NoError(t, err)
-
-				return len(svcAccs.Items) > 0
-			}, 5*time.Second, time.Second)
-
-			addRoomsRequest := &maestroApiV1.AddRoomsRequest{
-				SchedulerName: schedulerName,
-				Amount:        1,
-			}
+			addRoomsRequest := &maestroApiV1.AddRoomsRequest{SchedulerName: schedulerName, Amount: 1}
 			addRoomsResponse := &maestroApiV1.AddRoomsResponse{}
 			err = apiClient.Do("POST", fmt.Sprintf("/schedulers/%s/add-rooms", schedulerName), addRoomsRequest, addRoomsResponse)
 
@@ -143,82 +85,15 @@ func TestAddRooms(t *testing.T) {
 
 		t.Run("when created rooms replies its state back then it finishes the operation successfully", func(t *testing.T) {
 
-			schedulerName := framework.GenerateSchedulerName()
-			createRequest := &maestroApiV1.CreateSchedulerRequest{
-				Name:                   schedulerName,
-				Game:                   "test",
-				Version:                "v1.1",
-				TerminationGracePeriod: 15,
-				Containers: []*maestroApiV1.Container{
-					{
-						Name:  "example",
-						Image: "alpine",
-						Command: []string{"/bin/sh", "-c", "apk add curl && curl --request POST " +
-							"$ROOMS_API_ADDRESS:9097/scheduler/$MAESTRO_SCHEDULER_NAME/rooms/$MAESTRO_ROOM_ID/ping " +
-							"--data-raw '{\"status\": \"ready\",\"timestamp\": \"12312312313\"}'"},
-						ImagePullPolicy: "Always",
-						Environment: []*maestroApiV1.ContainerEnvironment{
-							{
-								Name:  "ROOMS_API_ADDRESS",
-								Value: maestro.RoomsApiServer.ContainerInternalAddress,
-							},
-						},
-						Requests: &maestroApiV1.ContainerResources{
-							Memory: "20Mi",
-							Cpu:    "10m",
-						},
-						Limits: &maestroApiV1.ContainerResources{
-							Memory: "20Mi",
-							Cpu:    "10m",
-						},
-						Ports: []*maestroApiV1.ContainerPort{
-							{
-								Name:     "default",
-								Protocol: "tcp",
-								Port:     80,
-							},
-						},
-					},
-				},
-			}
+			schedulerName, err := createSchedulerAndWaitForIt(t,
+				maestro,
+				apiClient,
+				kubeclient,
+				[]string{"/bin/sh", "-c", "apk add curl && curl --request POST " +
+					"$ROOMS_API_ADDRESS:9097/scheduler/$MAESTRO_SCHEDULER_NAME/rooms/$MAESTRO_ROOM_ID/ping " +
+					"--data-raw '{\"status\": \"ready\",\"timestamp\": \"12312312313\"}'"})
 
-			createResponse := &maestroApiV1.CreateSchedulerResponse{}
-			err := apiClient.Do("POST", "/schedulers", createRequest, createResponse)
-			require.NoError(t, err)
-
-			require.Eventually(t, func() bool {
-				listOperationsRequest := &maestroApiV1.ListOperationsRequest{}
-				listOperationsResponse := &maestroApiV1.ListOperationsResponse{}
-				err = apiClient.Do("GET", fmt.Sprintf("/schedulers/%s/operations", schedulerName), listOperationsRequest, listOperationsResponse)
-				require.NoError(t, err)
-
-				if len(listOperationsResponse.FinishedOperations) == 0 {
-					return false
-				}
-
-				require.Equal(t, "create_scheduler", listOperationsResponse.FinishedOperations[0].DefinitionName)
-				require.Equal(t, "finished", listOperationsResponse.FinishedOperations[0].Status)
-				return true
-			}, 30*time.Second, time.Second)
-
-			// Check on kubernetes that the scheduler namespace was created.
-			_, err = kubeclient.CoreV1().Namespaces().Get(context.Background(), schedulerName, metav1.GetOptions{})
-			require.NoError(t, err)
-
-			// wait for service account to be created
-			// TODO: check if we need to wait the service account to be created on internal/adapters/runtime/kubernetes/scheduler.go
-			// we were having errors when not waiting for this in this test, reported in this issue https://github.com/kubernetes/kubernetes/issues/66689
-			require.Eventually(t, func() bool {
-				svcAccs, err := kubeclient.CoreV1().ServiceAccounts(schedulerName).List(context.Background(), metav1.ListOptions{})
-				require.NoError(t, err)
-
-				return len(svcAccs.Items) > 0
-			}, 5*time.Second, time.Second)
-
-			addRoomsRequest := &maestroApiV1.AddRoomsRequest{
-				SchedulerName: schedulerName,
-				Amount:        1,
-			}
+			addRoomsRequest := &maestroApiV1.AddRoomsRequest{SchedulerName: schedulerName, Amount: 1}
 			addRoomsResponse := &maestroApiV1.AddRoomsResponse{}
 			err = apiClient.Do("POST", fmt.Sprintf("/schedulers/%s/add-rooms", schedulerName), addRoomsRequest, addRoomsResponse)
 
@@ -256,4 +131,82 @@ func TestAddRooms(t *testing.T) {
 		})
 	})
 
+}
+
+func createSchedulerAndWaitForIt(
+	t *testing.T,
+	maestro *maestro.MaestroInstance,
+	apiClient *framework.APIClient,
+	kubeclient kubernetes.Interface,
+	gruCommand []string) (string, error) {
+	schedulerName := framework.GenerateSchedulerName()
+	createRequest := &maestroApiV1.CreateSchedulerRequest{
+		Name:                   schedulerName,
+		Game:                   "test",
+		Version:                "v1.1",
+		TerminationGracePeriod: 15,
+		Containers: []*maestroApiV1.Container{
+			{
+				Name:            "example",
+				Image:           "alpine",
+				Command:         gruCommand,
+				ImagePullPolicy: "Always",
+				Environment: []*maestroApiV1.ContainerEnvironment{
+					{
+						Name:  "ROOMS_API_ADDRESS",
+						Value: maestro.RoomsApiServer.ContainerInternalAddress,
+					},
+				},
+				Requests: &maestroApiV1.ContainerResources{
+					Memory: "20Mi",
+					Cpu:    "10m",
+				},
+				Limits: &maestroApiV1.ContainerResources{
+					Memory: "20Mi",
+					Cpu:    "10m",
+				},
+				Ports: []*maestroApiV1.ContainerPort{
+					{
+						Name:     "default",
+						Protocol: "tcp",
+						Port:     80,
+					},
+				},
+			},
+		},
+	}
+
+	createResponse := &maestroApiV1.CreateSchedulerResponse{}
+	err := apiClient.Do("POST", "/schedulers", createRequest, createResponse)
+	require.NoError(t, err)
+
+	require.Eventually(t, func() bool {
+		listOperationsRequest := &maestroApiV1.ListOperationsRequest{}
+		listOperationsResponse := &maestroApiV1.ListOperationsResponse{}
+		err = apiClient.Do("GET", fmt.Sprintf("/schedulers/%s/operations", schedulerName), listOperationsRequest, listOperationsResponse)
+		require.NoError(t, err)
+
+		if len(listOperationsResponse.FinishedOperations) == 0 {
+			return false
+		}
+
+		require.Equal(t, "create_scheduler", listOperationsResponse.FinishedOperations[0].DefinitionName)
+		require.Equal(t, "finished", listOperationsResponse.FinishedOperations[0].Status)
+		return true
+	}, 30*time.Second, time.Second)
+
+	// Check on kubernetes that the scheduler namespace was created.
+	_, err = kubeclient.CoreV1().Namespaces().Get(context.Background(), schedulerName, metav1.GetOptions{})
+	require.NoError(t, err)
+
+	// wait for service account to be created
+	// TODO: check if we need to wait the service account to be created on internal/adapters/runtime/kubernetes/scheduler.go
+	// we were having errors when not waiting for this in this test, reported in this issue https://github.com/kubernetes/kubernetes/issues/66689
+	require.Eventually(t, func() bool {
+		svcAccs, err := kubeclient.CoreV1().ServiceAccounts(schedulerName).List(context.Background(), metav1.ListOptions{})
+		require.NoError(t, err)
+
+		return len(svcAccs.Items) > 0
+	}, 5*time.Second, time.Second)
+	return schedulerName, err
 }
