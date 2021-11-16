f19b3f7fab0f37c0e51d1aeefed72564610d4e78
“Caio <caiordjesus@gmail.com>
2021-11-16 13:53:07 -0300

    feat: getScheduler - generated files

--- pkg/api/v1/schedulers.pb.gw.go
+++ pkg/api/v1/schedulers.pb.gw.go
@@ -49,6 +49,58 @@ func local_request_SchedulersService_ListSchedulers_0(ctx context.Context, marsh
 
 }
 
+func request_SchedulersService_GetScheduler_0(ctx context.Context, marshaler runtime.Marshaler, client SchedulersServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq GetSchedulerRequest
+	var metadata runtime.ServerMetadata
+
+	var (
+		val string
+		ok  bool
+		err error
+		_   = err
+	)
+
+	val, ok = pathParams["scheduler_name"]
+	if !ok {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "scheduler_name")
+	}
+
+	protoReq.SchedulerName, err = runtime.String(val)
+	if err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "scheduler_name", err)
+	}
+
+	msg, err := client.GetScheduler(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
+func local_request_SchedulersService_GetScheduler_0(ctx context.Context, marshaler runtime.Marshaler, server SchedulersServiceServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq GetSchedulerRequest
+	var metadata runtime.ServerMetadata
+
+	var (
+		val string
+		ok  bool
+		err error
+		_   = err
+	)
+
+	val, ok = pathParams["scheduler_name"]
+	if !ok {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "scheduler_name")
+	}
+
+	protoReq.SchedulerName, err = runtime.String(val)
+	if err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "scheduler_name", err)
+	}
+
+	msg, err := server.GetScheduler(ctx, &protoReq)
+	return msg, metadata, err
+
+}
+
 func request_SchedulersService_CreateScheduler_0(ctx context.Context, marshaler runtime.Marshaler, client SchedulersServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq CreateSchedulerRequest
 	var metadata runtime.ServerMetadata
@@ -248,6 +300,29 @@ func RegisterSchedulersServiceHandlerServer(ctx context.Context, mux *runtime.Se
 
 	})
 
+	mux.Handle("GET", pattern_SchedulersService_GetScheduler_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		var stream runtime.ServerTransportStream
+		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateIncomingContext(ctx, mux, req, "/api.v1.SchedulersService/GetScheduler", runtime.WithHTTPPathPattern("/schedulers/{scheduler_name=*}"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := local_request_SchedulersService_GetScheduler_0(rctx, inboundMarshaler, server, req, pathParams)
+		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_SchedulersService_GetScheduler_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	mux.Handle("POST", pattern_SchedulersService_CreateScheduler_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
@@ -378,6 +453,26 @@ func RegisterSchedulersServiceHandlerClient(ctx context.Context, mux *runtime.Se
 
 	})
 
+	mux.Handle("GET", pattern_SchedulersService_GetScheduler_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req, "/api.v1.SchedulersService/GetScheduler", runtime.WithHTTPPathPattern("/schedulers/{scheduler_name=*}"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_SchedulersService_GetScheduler_0(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_SchedulersService_GetScheduler_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	mux.Handle("POST", pattern_SchedulersService_CreateScheduler_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
@@ -444,6 +539,8 @@ func RegisterSchedulersServiceHandlerClient(ctx context.Context, mux *runtime.Se
 var (
 	pattern_SchedulersService_ListSchedulers_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0}, []string{"schedulers"}, ""))
 
+	pattern_SchedulersService_GetScheduler_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 1, 0, 4, 1, 5, 1}, []string{"schedulers", "scheduler_name"}, ""))
+
 	pattern_SchedulersService_CreateScheduler_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0}, []string{"schedulers"}, ""))
 
 	pattern_SchedulersService_AddRooms_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 1, 0, 4, 1, 5, 1, 2, 2}, []string{"schedulers", "scheduler_name", "add-rooms"}, ""))
@@ -453,6 +550,8 @@ var (
 
 var (
 	forward_SchedulersService_ListSchedulers_0 = runtime.ForwardResponseMessage
+
+	forward_SchedulersService_GetScheduler_0 = runtime.ForwardResponseMessage
 
 	forward_SchedulersService_CreateScheduler_0 = runtime.ForwardResponseMessage
 
