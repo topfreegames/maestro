e0a37c59f8eef59029c8eb9f12fe11d13410ec01
“Caio <caiordjesus@gmail.com>
2022-02-24 17:43:56 -0300

    chore(game_room): refactor rooms handler to use room manager mock

--- internal/api/handlers/rooms_handler_test.go
+++ internal/api/handlers/rooms_handler_test.go
@@ -26,35 +26,24 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	mockeventsservice "github.com/topfreegames/maestro/internal/core/services/interfaces/mock/events_service"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"os"
 	"testing"
-	"time"
 
-	mockeventsservice "github.com/topfreegames/maestro/internal/core/services/interfaces/mock/events_service"
-
-	"github.com/topfreegames/maestro/internal/core/entities/game_room"
 	"github.com/topfreegames/maestro/internal/core/ports/errors"
-
-	clock_mock "github.com/topfreegames/maestro/internal/adapters/clock/mock"
 
 	"github.com/golang/mock/gomock"
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"github.com/stretchr/testify/require"
-	instance_storage_mock "github.com/topfreegames/maestro/internal/adapters/instance_storage/mock"
-	port_allocator_mock "github.com/topfreegames/maestro/internal/adapters/port_allocator/mock"
-	runtime_mock "github.com/topfreegames/maestro/internal/adapters/runtime/mock"
-	"github.com/topfreegames/maestro/internal/core/ports/mock"
-	"github.com/topfreegames/maestro/internal/core/services/room_manager"
+	mockports "github.com/topfreegames/maestro/internal/core/ports/mock"
 	api "github.com/topfreegames/maestro/pkg/api/v1"
 )
 
 func TestRoomsHandler_UpdateRoomWithPing(t *testing.T) {
 	dirPath, _ := os.Getwd()
-
-	instance := &game_room.Instance{Status: game_room.InstanceStatus{Type: game_room.InstanceReady}}
 
 	validRequests, _ := ioutil.ReadFile(dirPath + "/fixtures/request/valid-ping-data-list.json")
 	var validRawRequests []*json.RawMessage
@@ -66,38 +55,18 @@ func TestRoomsHandler_UpdateRoomWithPing(t *testing.T) {
 	err = json.Unmarshal(invalidStateRequests, &invalidStateRawRequests)
 	require.NoError(t, err)
 
-	t.Run("with valid request and existent game room it should return status code 200 with success = true", func(t *testing.T) {
+	t.Run("should succeed - valid request, existent game room => return status code 200", func(t *testing.T) {
 		mockCtrl := gomock.NewController(t)
 
-		clockMock := clock_mock.NewFakeClock(time.Now())
-		portAllocatorMock := port_allocator_mock.NewMockPortAllocator(mockCtrl)
-		roomStorageMock := mock.NewMockRoomStorage(mockCtrl)
-		instanceStorageMock := instance_storage_mock.NewMockGameRoomInstanceStorage(mockCtrl)
 		eventsForwarderService := mockeventsservice.NewMockEventsService(mockCtrl)
-		runtimeMock := runtime_mock.NewMockRuntime(mockCtrl)
-		config := room_manager.RoomManagerConfig{RoomInitializationTimeout: time.Millisecond * 1000}
 
-		roomsManager := room_manager.New(clockMock, portAllocatorMock, roomStorageMock, instanceStorageMock, runtimeMock, eventsForwarderService, config)
+		roomsManager := mockports.NewMockRoomManager(mockCtrl)
 		mux := runtime.NewServeMux()
 		err := api.RegisterRoomsServiceHandlerServer(context.Background(), mux, ProvideRoomsHandler(roomsManager, eventsForwarderService))
 		require.NoError(t, err)
 
 		for _, validRawRequest := range validRawRequests {
-			// TODO(gabrielcorado): since we're exposing the room manager
-			// internals we have to do it to mock it properly
-			var updatedGameRoom *game_room.GameRoom
-			roomStorageMock.EXPECT().UpdateRoom(gomock.Any(), gomock.Any()).DoAndReturn(func(_ context.Context, receivedGameRoom *game_room.GameRoom) error {
-				receivedGameRoom.Status = game_room.GameStatusReady
-				updatedGameRoom = receivedGameRoom
-				return nil
-			})
-
-			instanceStorageMock.EXPECT().GetInstance(gomock.Any(), gomock.Any(), gomock.Any()).Return(instance, nil)
-			roomStorageMock.EXPECT().GetRoom(gomock.Any(), gomock.Any(), gomock.Any()).DoAndReturn(func(_ context.Context, _ string, _ string) (*game_room.GameRoom, error) {
-				return updatedGameRoom, nil
-			})
-			roomStorageMock.EXPECT().UpdateRoomStatus(gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any()).Return(nil)
-			eventsForwarderService.EXPECT().ProduceEvent(gomock.Any(), gomock.Any())
+			roomsManager.EXPECT().UpdateRoom(gomock.Any(), gomock.Any()).Return(nil)
 
 			request, err := validRawRequest.MarshalJSON()
 			require.NoError(t, err)
@@ -114,26 +83,18 @@ func TestRoomsHandler_UpdateRoomWithPing(t *testing.T) {
 		}
 	})
 
-	t.Run("with valid request and nonexistent game room then it should return with status code 404", func(t *testing.T) {
-		// TODO(gabrielcorado): we're skipping this test since the update room
-		// currently doesn't fail if the room doesn't exists.
-		t.Skip()
+	t.Run("should fail - valid request, non-existent game room => return status code 404", func(t *testing.T) {
 
 		mockCtrl := gomock.NewController(t)
 
-		clockMock := clock_mock.NewFakeClock(time.Now())
-		portAllocatorMock := port_allocator_mock.NewMockPortAllocator(mockCtrl)
-		roomStorageMock := mock.NewMockRoomStorage(mockCtrl)
-		instanceStorageMock := instance_storage_mock.NewMockGameRoomInstanceStorage(mockCtrl)
 		eventsForwarderService := mockeventsservice.NewMockEventsService(mockCtrl)
-		runtimeMock := runtime_mock.NewMockRuntime(mockCtrl)
-		config := room_manager.RoomManagerConfig{RoomInitializationTimeout: time.Millisecond * 1000}
-		roomsManager := room_manager.New(clockMock, portAllocatorMock, roomStorageMock, instanceStorageMock, runtimeMock, eventsForwarderService, config)
+
+		roomsManager := mockports.NewMockRoomManager(mockCtrl)
 		mux := runtime.NewServeMux()
 		err := api.RegisterRoomsServiceHandlerServer(context.Background(), mux, ProvideRoomsHandler(roomsManager, eventsForwarderService))
 		require.NoError(t, err)
 
-		roomStorageMock.EXPECT().UpdateRoom(gomock.Any(), gomock.Any()).Return(errors.ErrNotFound)
+		roomsManager.EXPECT().UpdateRoom(gomock.Any(), gomock.Any()).Return(errors.NewErrNotFound("NOT FOUND"))
 
 		request, err := validRawRequests[0].MarshalJSON()
 		require.NoError(t, err)
@@ -147,22 +108,17 @@ func TestRoomsHandler_UpdateRoomWithPing(t *testing.T) {
 		require.Equal(t, 404, rr.Code)
 	})
 
-	t.Run("with valid request when have error while updating game room then it should return with status code 500", func(t *testing.T) {
+	t.Run("should fail - valid request, error while updating game room => return status code 500", func(t *testing.T) {
 		mockCtrl := gomock.NewController(t)
 
-		clockMock := clock_mock.NewFakeClock(time.Now())
-		portAllocatorMock := port_allocator_mock.NewMockPortAllocator(mockCtrl)
-		roomStorageMock := mock.NewMockRoomStorage(mockCtrl)
-		instanceStorageMock := instance_storage_mock.NewMockGameRoomInstanceStorage(mockCtrl)
 		eventsForwarderService := mockeventsservice.NewMockEventsService(mockCtrl)
-		runtimeMock := runtime_mock.NewMockRuntime(mockCtrl)
-		config := room_manager.RoomManagerConfig{RoomInitializationTimeout: time.Millisecond * 1000}
-		roomsManager := room_manager.New(clockMock, portAllocatorMock, roomStorageMock, instanceStorageMock, runtimeMock, eventsForwarderService, config)
+
+		roomsManager := mockports.NewMockRoomManager(mockCtrl)
 		mux := runtime.NewServeMux()
 		err := api.RegisterRoomsServiceHandlerServer(context.Background(), mux, ProvideRoomsHandler(roomsManager, eventsForwarderService))
 		require.NoError(t, err)
 
-		roomStorageMock.EXPECT().UpdateRoom(gomock.Any(), gomock.Any()).Return(errors.ErrUnexpected)
+		roomsManager.EXPECT().UpdateRoom(gomock.Any(), gomock.Any()).Return(errors.ErrUnexpected)
 
 		request, err := validRawRequests[0].MarshalJSON()
 		require.NoError(t, err)
@@ -176,60 +132,12 @@ func TestRoomsHandler_UpdateRoomWithPing(t *testing.T) {
 		require.Equal(t, 500, rr.Code)
 	})
 
-	t.Run("with valid request when the new game room state transition is invalid then it should return with status code 500", func(t *testing.T) {
+	t.Run("should fail - invalid request => return status code 400", func(t *testing.T) {
 		mockCtrl := gomock.NewController(t)
 
-		clockMock := clock_mock.NewFakeClock(time.Now())
-		portAllocatorMock := port_allocator_mock.NewMockPortAllocator(mockCtrl)
-		roomStorageMock := mock.NewMockRoomStorage(mockCtrl)
-		instanceStorageMock := instance_storage_mock.NewMockGameRoomInstanceStorage(mockCtrl)
 		eventsForwarderService := mockeventsservice.NewMockEventsService(mockCtrl)
-		runtimeMock := runtime_mock.NewMockRuntime(mockCtrl)
-		config := room_manager.RoomManagerConfig{RoomInitializationTimeout: time.Millisecond * 1000}
-		roomsManager := room_manager.New(clockMock, portAllocatorMock, roomStorageMock, instanceStorageMock, runtimeMock, eventsForwarderService, config)
-		mux := runtime.NewServeMux()
-		err := api.RegisterRoomsServiceHandlerServer(context.Background(), mux, ProvideRoomsHandler(roomsManager, eventsForwarderService))
-		require.NoError(t, err)
 
-		for _, invalidStateRawRequest := range invalidStateRawRequests {
-			// TODO(gabrielcorado): since we're exposing the room manager
-			// internals we have to do it to mock it properly
-			var updatedGameRoom *game_room.GameRoom
-			roomStorageMock.EXPECT().UpdateRoom(gomock.Any(), gomock.Any()).DoAndReturn(func(_ context.Context, receivedGameRoom *game_room.GameRoom) error {
-				receivedGameRoom.Status = game_room.GameStatusTerminating
-				updatedGameRoom = receivedGameRoom
-				return nil
-			})
-
-			instanceStorageMock.EXPECT().GetInstance(gomock.Any(), gomock.Any(), gomock.Any()).Return(instance, nil)
-			roomStorageMock.EXPECT().GetRoom(gomock.Any(), gomock.Any(), gomock.Any()).DoAndReturn(func(_ context.Context, _ string, _ string) (*game_room.GameRoom, error) {
-				return updatedGameRoom, nil
-			})
-
-			request, err := invalidStateRawRequest.MarshalJSON()
-			require.NoError(t, err)
-
-			req, err := http.NewRequest(http.MethodPost, "/scheduler/scheduler-name-1/rooms/room-name-1/ping", bytes.NewReader(request))
-			require.NoError(t, err)
-
-			rr := httptest.NewRecorder()
-			mux.ServeHTTP(rr, req)
-
-			require.Equal(t, 500, rr.Code)
-		}
-	})
-
-	t.Run("with invalid request then it should return with status code 400", func(t *testing.T) {
-		mockCtrl := gomock.NewController(t)
-
-		clockMock := clock_mock.NewFakeClock(time.Now())
-		portAllocatorMock := port_allocator_mock.NewMockPortAllocator(mockCtrl)
-		roomStorageMock := mock.NewMockRoomStorage(mockCtrl)
-		instanceStorageMock := instance_storage_mock.NewMockGameRoomInstanceStorage(mockCtrl)
-		eventsForwarderService := mockeventsservice.NewMockEventsService(mockCtrl)
-		runtimeMock := runtime_mock.NewMockRuntime(mockCtrl)
-		config := room_manager.RoomManagerConfig{RoomInitializationTimeout: time.Millisecond * 1000}
-		roomsManager := room_manager.New(clockMock, portAllocatorMock, roomStorageMock, instanceStorageMock, runtimeMock, eventsForwarderService, config)
+		roomsManager := mockports.NewMockRoomManager(mockCtrl)
 		mux := runtime.NewServeMux()
 		err := api.RegisterRoomsServiceHandlerServer(context.Background(), mux, ProvideRoomsHandler(roomsManager, eventsForwarderService))
 		require.NoError(t, err)
@@ -255,18 +163,12 @@ func TestRoomsHandler_ForwardRoomEvent(t *testing.T) {
 	err := json.Unmarshal(requests, &rawRequests)
 	require.NoError(t, err)
 
-	t.Run("when no error occur when forwarding then it return status code 200 with success = true", func(t *testing.T) {
+	t.Run("should succeed - no error occur when forwarding => returns status code 200", func(t *testing.T) {
 		mockCtrl := gomock.NewController(t)
 
-		clockMock := clock_mock.NewFakeClock(time.Now())
-		portAllocatorMock := port_allocator_mock.NewMockPortAllocator(mockCtrl)
-		roomStorageMock := mock.NewMockRoomStorage(mockCtrl)
-		instanceStorageMock := instance_storage_mock.NewMockGameRoomInstanceStorage(mockCtrl)
 		eventsForwarderService := mockeventsservice.NewMockEventsService(mockCtrl)
-		runtimeMock := runtime_mock.NewMockRuntime(mockCtrl)
-		config := room_manager.RoomManagerConfig{RoomInitializationTimeout: time.Millisecond * 1000}
 
-		roomsManager := room_manager.New(clockMock, portAllocatorMock, roomStorageMock, instanceStorageMock, runtimeMock, eventsForwarderService, config)
+		roomsManager := mockports.NewMockRoomManager(mockCtrl)
 		mux := runtime.NewServeMux()
 		err := api.RegisterRoomsServiceHandlerServer(context.Background(), mux, ProvideRoomsHandler(roomsManager, eventsForwarderService))
 		require.NoError(t, err)
@@ -296,15 +198,9 @@ func TestRoomsHandler_ForwardRoomEvent(t *testing.T) {
 	t.Run("when some error occur when forwarding then it return status code 200 with success = false", func(t *testing.T) {
 		mockCtrl := gomock.NewController(t)
 
-		clockMock := clock_mock.NewFakeClock(time.Now())
-		portAllocatorMock := port_allocator_mock.NewMockPortAllocator(mockCtrl)
-		roomStorageMock := mock.NewMockRoomStorage(mockCtrl)
-		instanceStorageMock := instance_storage_mock.NewMockGameRoomInstanceStorage(mockCtrl)
 		eventsForwarderService := mockeventsservice.NewMockEventsService(mockCtrl)
-		runtimeMock := runtime_mock.NewMockRuntime(mockCtrl)
-		config := room_manager.RoomManagerConfig{RoomInitializationTimeout: time.Millisecond * 1000}
 
-		roomsManager := room_manager.New(clockMock, portAllocatorMock, roomStorageMock, instanceStorageMock, runtimeMock, eventsForwarderService, config)
+		roomsManager := mockports.NewMockRoomManager(mockCtrl)
 		mux := runtime.NewServeMux()
 		err := api.RegisterRoomsServiceHandlerServer(context.Background(), mux, ProvideRoomsHandler(roomsManager, eventsForwarderService))
 		require.NoError(t, err)
@@ -341,15 +237,9 @@ func TestRoomsHandler_ForwardPlayerEvent(t *testing.T) {
 	t.Run("when no error occur when forwarding then it return status code 200 with success = true", func(t *testing.T) {
 		mockCtrl := gomock.NewController(t)
 
-		clockMock := clock_mock.NewFakeClock(time.Now())
-		portAllocatorMock := port_allocator_mock.NewMockPortAllocator(mockCtrl)
-		roomStorageMock := mock.NewMockRoomStorage(mockCtrl)
-		instanceStorageMock := instance_storage_mock.NewMockGameRoomInstanceStorage(mockCtrl)
 		eventsForwarderService := mockeventsservice.NewMockEventsService(mockCtrl)
-		runtimeMock := runtime_mock.NewMockRuntime(mockCtrl)
-		config := room_manager.RoomManagerConfig{RoomInitializationTimeout: time.Millisecond * 1000}
 
-		roomsManager := room_manager.New(clockMock, portAllocatorMock, roomStorageMock, instanceStorageMock, runtimeMock, eventsForwarderService, config)
+		roomsManager := mockports.NewMockRoomManager(mockCtrl)
 		mux := runtime.NewServeMux()
 		err := api.RegisterRoomsServiceHandlerServer(context.Background(), mux, ProvideRoomsHandler(roomsManager, eventsForwarderService))
 		require.NoError(t, err)
@@ -378,15 +268,9 @@ func TestRoomsHandler_ForwardPlayerEvent(t *testing.T) {
 	t.Run("when some error occur when forwarding then it return status code 200 with success = false", func(t *testing.T) {
 		mockCtrl := gomock.NewController(t)
 
-		clockMock := clock_mock.NewFakeClock(time.Now())
-		portAllocatorMock := port_allocator_mock.NewMockPortAllocator(mockCtrl)
-		roomStorageMock := mock.NewMockRoomStorage(mockCtrl)
-		instanceStorageMock := instance_storage_mock.NewMockGameRoomInstanceStorage(mockCtrl)
 		eventsForwarderService := mockeventsservice.NewMockEventsService(mockCtrl)
-		runtimeMock := runtime_mock.NewMockRuntime(mockCtrl)
-		config := room_manager.RoomManagerConfig{RoomInitializationTimeout: time.Millisecond * 1000}
 
-		roomsManager := room_manager.New(clockMock, portAllocatorMock, roomStorageMock, instanceStorageMock, runtimeMock, eventsForwarderService, config)
+		roomsManager := mockports.NewMockRoomManager(mockCtrl)
 		mux := runtime.NewServeMux()
 		err := api.RegisterRoomsServiceHandlerServer(context.Background(), mux, ProvideRoomsHandler(roomsManager, eventsForwarderService))
 		require.NoError(t, err)
@@ -417,15 +301,9 @@ func TestRoomsHandler_UpdateRoomStatus(t *testing.T) {
 	t.Run("it does nothing and returns 200 ok with success equal true for all requests", func(t *testing.T) {
 		mockCtrl := gomock.NewController(t)
 
-		clockMock := clock_mock.NewFakeClock(time.Now())
-		portAllocatorMock := port_allocator_mock.NewMockPortAllocator(mockCtrl)
-		roomStorageMock := mock.NewMockRoomStorage(mockCtrl)
-		instanceStorageMock := instance_storage_mock.NewMockGameRoomInstanceStorage(mockCtrl)
 		eventsForwarderService := mockeventsservice.NewMockEventsService(mockCtrl)
-		runtimeMock := runtime_mock.NewMockRuntime(mockCtrl)
-		config := room_manager.RoomManagerConfig{RoomInitializationTimeout: time.Millisecond * 1000}
 
-		roomsManager := room_manager.New(clockMock, portAllocatorMock, roomStorageMock, instanceStorageMock, runtimeMock, eventsForwarderService, config)
+		roomsManager := mockports.NewMockRoomManager(mockCtrl)
 		mux := runtime.NewServeMux()
 		err := api.RegisterRoomsServiceHandlerServer(context.Background(), mux, ProvideRoomsHandler(roomsManager, eventsForwarderService))
 		require.NoError(t, err)
@@ -445,4 +323,5 @@ func TestRoomsHandler_UpdateRoomStatus(t *testing.T) {
 		require.NoError(t, err)
 		require.Equal(t, true, body["success"])
 	})
+
 }
