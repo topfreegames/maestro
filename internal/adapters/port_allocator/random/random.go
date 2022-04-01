5ce04971175db3b8722868b80ea60027f0934e8f
GuilhermBrSp <guilhermbrsp@gmail.com>
2022-04-01 13:45:29 -0300

    Remove random source that is not concurrent-safe

--- internal/adapters/port_allocator/random/random.go
+++ internal/adapters/port_allocator/random/random.go
@@ -23,25 +23,21 @@
 package random
 
 import (
-	"math/rand"
-	"time"
-
 	"github.com/topfreegames/maestro/internal/core/entities"
 	"github.com/topfreegames/maestro/internal/core/ports"
 	"github.com/topfreegames/maestro/internal/core/ports/errors"
+	"math/rand"
 )
 
 var _ ports.PortAllocator = (*RandomPortAllocator)(nil)
 
 type RandomPortAllocator struct {
-	random           *rand.Rand
 	defaultPortRange *entities.PortRange
 }
 
 func NewRandomPortAllocator(portRange *entities.PortRange) *RandomPortAllocator {
 	return &RandomPortAllocator{
 		defaultPortRange: portRange,
-		random:           rand.New(rand.NewSource(time.Now().Unix())),
 	}
 }
 
@@ -55,7 +51,7 @@ func (r *RandomPortAllocator) Allocate(portRange *entities.PortRange, quantity i
 	alreadyAllocated := make(map[int32]struct{})
 
 	for len(ports) < quantity {
-		port := currentRange.Start + r.random.Int31n(currentRange.Total())
+		port := currentRange.Start + rand.Int31n(currentRange.Total())
 		if _, ok := alreadyAllocated[port]; ok {
 			if len(alreadyAllocated) == int(currentRange.Total()) {
 				return ports, errors.NewErrInvalidArgument("not enough ports to allocate")
