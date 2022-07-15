9b0851bc2f00f11e8a109189e7aad96096c2e142
Mayhhara F Lilian <69424067+mflilian@users.noreply.github.com>
2022-07-15 15:13:20 -0300

    AddRooms req/res and RemoveRooms req/res should be removed (#494)
    * deletes remove/add-rooms-res/req operation from proto
    
    * runs make generate
    
    * deletes addroom/removeroom from scheduler files

--- internal/core/ports/scheduler_ports.go
+++ internal/core/ports/scheduler_ports.go
@@ -45,8 +45,6 @@ type SchedulerManager interface {
 	GetSchedulerVersions(ctx context.Context, schedulerName string) ([]*entities.SchedulerVersion, error)
 	DeleteScheduler(ctx context.Context, schedulerName string) error
 	PatchSchedulerAndCreateNewSchedulerVersionOperation(ctx context.Context, schedulerName string, patchMap map[string]interface{}) (*operation.Operation, error)
-	AddRooms(ctx context.Context, schedulerName string, amount int32) (*operation.Operation, error)
-	RemoveRooms(ctx context.Context, schedulerName string, amount int) (*operation.Operation, error)
 	GetSchedulersWithFilter(ctx context.Context, schedulerFilter *filters.SchedulerFilter) ([]*entities.Scheduler, error)
 	GetScheduler(ctx context.Context, schedulerName, version string) (*entities.Scheduler, error)
 	EnqueueNewSchedulerVersionOperation(ctx context.Context, scheduler *entities.Scheduler) (*operation.Operation, error)
