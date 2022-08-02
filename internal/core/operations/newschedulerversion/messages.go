6f42019a3a4a4ea40b224812e6d2cfe9a25854c1
GuilhermBrSp <guilhermbrsp@gmail.com>
2022-08-02 15:24:39 -0300

    Fix messages typos

--- internal/core/operations/newschedulerversion/messages.go
+++ internal/core/operations/newschedulerversion/messages.go
@@ -24,10 +24,10 @@ package newschedulerversion
 
 const (
 	validationTimeoutMessage = `The GRU could not be validated. Maestro got timeout waiting for the GRU with ID: %s to be ready. You can check if
-		the GRU image is stable on the its logs. If you could not spot any issues, contact the Maestro's responsible team for helping.`
+		the GRU image is stable on its logs. If you could not spot any issues, contact the Maestro's responsible team for helping.`
 
 	validationPodInErrorMessage = `The GRU could not be validated. The room created for validation with ID %s is entering in error state. You can check if
-		the GRU image is stable on the its logs using the provided room id. Last event in the game room: %s.`
+		the GRU image is stable on its logs using the provided room id. Last event in the game room: %s.`
 
 	validationUnexpectedErrorMessage = `The GRU could not be validated. Unexpected Error: %s - Contact the Maestro's responsible team for helping.`
 )
